d23e36466b8874541b75c6c263d7571ee0dd2a91
orangeQWJ <intqwj@gmail.com>
2024-06-25 16:14:59 +0800

    为wrk 准备压力测试脚本

---
+++ scripts/wrk/login.lua
@@ -0,0 +1,4 @@
+wrk.method="POST"
+wrk.headers["Content-Type"] = "application/json"
+-- 这个要改为你的注册的数据
+wrk.body='{"email":"wenjuqi2017@163.com", "password": "hello#world8725"}'
---
+++ scripts/wrk/profile.lua
@@ -0,0 +1,5 @@
+wrk.method="GET"
+wrk.headers["Content-Type"] = "application/json"
+wrk.headers["User-Agent"] = "PostmanRuntime/7.39.0"
+-- 记得修改这个，你在登录页面登录一下，然后复制一个过来这里
+wrk.headers["Authorization"]="Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTkzMDQyNDAsIlVpZCI6MSwiVXNlckFnZW50IjoiUG9zdG1hblJ1bnRpbWUvNy4zOS4wIn0.ReHAxbwfGn8JNf4EQuVd0koFsqwu9zL_esgnl1Gyp-AiByw0Sj5RYNTpXFnVYQ7ALTrlVSQu1Nf-WkkkVbjR3w"
